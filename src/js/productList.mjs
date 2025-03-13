9234fa6f97719e0924345affa8e646c9fbe56eea
chighwood <hig23003@byui.edu>
2025-03-13 18:01:18 -0500

    Changes for Week 2 solo

---
+++ src/js/productList.mjs
@@ -0,0 +1,32 @@
+function productCardTemplate(product) {
+    return `<div class="product-card">
+      <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}"/>
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </div>`;
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+
+    renderList(list) {
+        const htmlStrings = list.map(productCardTemplate);
+        this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
+    }
+}
