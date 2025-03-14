6f1f7fd7ccbfdd53fd4e9e526ea8255d0aa93d98
chighwood <hig23003@byui.edu>
2025-03-13 19:23:12 -0500

    Additional Changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './productList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./productList.mjs";
 
-const element = document.querySelector("#product-list");
+const element = document.querySelector(".product-list");
 const dataSource = new ProductData("tents");
 const listing = new ProductList("Tents", dataSource, element);
 
-productData.getData().then((data) => {
-    console.log("Loaded Product Data:", data);
-}).catch((error) => {
-    console.error("Error fetching product data:", error);
-});
+// ProductData.getData().then((data) => {
+//     console.log("Loaded Product Data:", data);
+// }).catch((error) => {
+//     console.error("Error fetching product data:", error);
+// });
 
 listing.init();
