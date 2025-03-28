70fe86cb7193f9686145bff34c0b2a74661b3dcd
Linguinese <nicolas.eiji.morise@gmail.com>
2025-03-27 22:47:30 -0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,4 @@ import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("Tents", dataSource, element);
 
-productList.init();
